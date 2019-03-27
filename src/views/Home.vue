55bc25bc7cb8aade6f990c339a7235d9dbc588e9
Yidoon <1471797269@qq.com>
2019-03-28 00:05:27 +0800

    init page

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <el-row>
+      <el-col :span="24">
+        <div class="header"></div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: "home",
+  components: {}
+};
+</script>
+
+<style lang="less" scoped>
+.home {
+  .header {
+    height: 20px;
+    border: 1px solid red;
+  }
+}
+</style>
+
